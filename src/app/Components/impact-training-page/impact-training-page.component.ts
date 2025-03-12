1eef3c43311b9fcfdbc727ecceec14ed964ce1d7
Manikandan <manideveloper2312@gmail.com>
2025-03-12 15:03:09 +0530

    Impact-Training Updated

--- src/app/Components/impact-training-page/impact-training-page.component.ts
+++ src/app/Components/impact-training-page/impact-training-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-impact-training-page',
@@ -6,5 +7,224 @@ import { Component } from '@angular/core';
   styleUrls: ['./impact-training-page.component.css']
 })
 export class ImpactTrainingPageComponent {
+ TNData:any =[];
+  checkTNData = [];
+  checkboxIndex= 1;
 
+  showMemberManagement:boolean = false;
+  showUploadFile:boolean = false;
+  showEditMember:boolean = false;
+  showTraineeList:boolean = true;
+
+  memberForm:FormGroup;
+  uploadForm:FormGroup;
+  KeywordForm:FormGroup;
+  editMemberForm:FormGroup;
+
+  filterByKeyword:string = "";
+  filterByACEID: string ="";
+  filterByFirstName:string="";
+  filterByLastName:string= "";
+  filterByDesignation:string="";
+  filterByTeam:string = "";
+
+
+  constructor(private formBuilder: FormBuilder) { 
+
+    // this FormGroup is used to filter the data by Keyword
+    this.KeywordForm = this.formBuilder.group({
+      filterByKeyword: new FormControl(['',Validators.required])
+    });
+
+    // this FormGroup is used to apply the conditions to the member Add Form
+    this.memberForm= this.formBuilder.group({
+
+      ACE_ID: ['',Validators.required],
+      First_Name: ['',Validators.required],
+      Last_Name: ['',Validators.required],
+      Designation: ['',Validators.required],
+      Team: ['',Validators.required],
+      Practice: ['',Validators.required]
+
+    });
+    // this FormGroup is used to apply the conditions to the member Edit Form
+    this.editMemberForm= this.formBuilder.group({
+
+      ACE_ID: ['',Validators.required],
+      First_Name: ['',Validators.required],
+      Mail_ID: ['',Validators.required],
+      College_Name: ['',Validators.required],
+      Practice: ['',Validators.required],
+      DOJ: ['',Validators.required]
+
+    });
+
+    // this FormGroup is used to apply the condition in File Upload Form
+    this.uploadForm = this.formBuilder.group({
+      fileUpload:['',Validators.required]
+    });
+
+    // this Array is the Full details of the TN Team
+    this.TNData=[
+      {
+        "ACE_ID":"ACE12391",
+        "First_Name": "Manikandan",
+        "Mail_ID": "mani.obulisamy@aspiresys.com",
+        "College_Name": "Knowledge Institute of Technology",
+        "Practice": "Testing",
+        "DOJ":"11-03-2025"
+      },
+      {
+        "ACE_ID":"ACE12393",
+        "First_Name": "Naveen",
+        "Mail_ID": "naveen.subramaniam@aspiresys.com",
+        "College_Name": "Knowledge Institute of Technology",
+        "Practice": "Testing",
+        "DOJ":"11-03-2025"
+      }
+    ]
+
+    this.checkTNData = this.TNData;
+    //this code is used when the filterByKeyword variable disturbed it calls filter function
+    this.KeywordForm.valueChanges.subscribe((value) => {
+      // if(value.filterByKeyword !==  '')
+        this.filterData(value);
+    });
+  }
+
+  // this method is used to show the member management
+  showMember(){
+    if(this.showMemberManagement){
+      this.showMemberManagement = false;
+    }else{
+      this.showMemberManagement = true;
+    }
+  }
+
+  //this method is used to clear the data in form
+  clearForm(){
+    this.memberForm.reset();
+    this.uploadForm.reset();
+  }
+
+  // this method is used to show the file upload form
+  showFileUpload(){
+    if(this.showUploadFile){
+      this.showUploadFile = false;
+    }else{
+      this.showUploadFile = true;
+    }
+  }
+
+
+  // this method is used to select all the checkboxes
+  clickAllCheckBoxes(){
+    for(var i=0;i<this.TNData.length;i++){
+      const checkbox = document.getElementById("checkbox"+i) as HTMLInputElement;
+      if(this.checkboxIndex == 1){
+        checkbox.checked = true;
+      }else{
+        checkbox.checked = false;
+      }
+
+    }
+    if(this.checkboxIndex == 1)
+    this.checkboxIndex = 2;
+    else
+    this.checkboxIndex = 1;
+  }
+
+
+  // this method is used to filter the data
+  filterData(value:any){
+        this.TNData = this.checkTNData;
+      if(value.filterByKeyword != ''){
+        let filtertedData = new Set<any>();
+        for(var i=0;i<this.TNData.length;i++){
+          var data = Object.values(this.TNData[i]);
+          for(var j=0;j<data.length;j++){
+            var checkData = String(data[j]).toLowerCase();
+          if(checkData.includes(value.filterByKeyword.toLowerCase())){
+            filtertedData.add(this.TNData[i]);
+          }
+        }
+        }
+
+
+        if(filtertedData.size == 0)
+          this.TNData = [];
+        else
+        this.TNData = [...filtertedData];
+    }
+  }
+
+  // this method is used to Add the Member
+  addMember(value: any){
+    this.TNData.push(value);
+    alert("successfully Added "+value.First_Name);
+    this.showMember();
+  }
+
+  // this method is used to remove the member
+  deleteMember(index:any){
+    if(confirm("sure you want to Remove "+this.TNData[index].First_Name))
+    this.TNData.splice(index,1);
+  }
+
+  // this method is used to show the Edit form
+  showEdit(){
+    if(this.showEditMember)
+      this.showEditMember = false;
+    else
+    this.showEditMember = true;
+  }
+
+  // this method is used to fill the Member Details in Edit Form
+  editMember(index:any){
+    this.showEdit();
+    this.editMemberForm.controls['ACE_ID'].setValue(this.TNData[index].ACE_ID);
+    this.editMemberForm.controls['First_Name'].setValue(this.TNData[index].First_Name);
+    this.editMemberForm.controls['Mail_ID'].setValue(this.TNData[index].Mail_ID);
+    this.editMemberForm.controls['College_Name'].setValue(this.TNData[index].College_Name);
+    this.editMemberForm.controls['Practice'].setValue(this.TNData[index].Practice);
+    this.editMemberForm.controls['DOJ'].setValue(this.TNData[index].DOJ);
+  }
+
+
+  // this method is used to Edit the Member Details
+  EditedMemberDetails(value:any){
+    
+    for(var i=0;i<this.TNData.length;i++){
+      if(this.TNData[i].ACE_ID === value.ACE_ID){
+        this.TNData[i] = value;
+        break;
+      }
+    }
+
+    this.showEdit();
+  }
+
+  // this method is used to delete all the members
+  deleteAllMembers(){
+    const checkbox = document.getElementById("parentCheckBox") as HTMLInputElement;
+    if(checkbox.checked){
+    if(confirm("sure you want to remove all Members")){
+    this.TNData = [];
+    checkbox.checked = false;
+    }
+    }else{
+      var checkedIndex = 0;
+      for(var i=0;i<this.TNData.length;i++){
+        const checkbox = document.getElementById("checkbox"+i) as HTMLInputElement;
+        if(checkbox.checked){
+          this.deleteMember(i);
+          ++checkedIndex;
+          checkbox.checked = false;
+        }
+  
+      }
+      if(checkedIndex == 0)
+        alert("select the Members to remove");
+    }
+  }
 }
